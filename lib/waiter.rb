e50a73af800ba7abb96238e04151a0b8fb3e5846
Nicole Dow <nikki.dow71@gmail.com>
2019-07-30 13:57:20 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,13 @@
 class Waiter
+  attr_accessor :name, :age, :yrs_experience
+  
+  @@all = []
+  
+  def initialize(name, yrs_experience)
+    @name = name
+    @age = age
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
 
 end
